6376406c9c34be0a94fb7bac42c8b1412e68e29f
shuchao <645904877@qq.com>
2017-06-12 12:05:54 +0800

    ...

--- http/http.js
+++ http/http.js
@@ -1,10 +1,10 @@
 var express = require("express");
-var session = require("../session/memory");
+var session = require("../session/memory").all();
 
 var http = express();
 
 http.post("/push/one", function (req, res) {
-    res.send(JSON.stringify(session.all()));
+    //res.send(JSON.stringify(session.all()));
 });
 
 http.post("/push/all", function (req, res) {
